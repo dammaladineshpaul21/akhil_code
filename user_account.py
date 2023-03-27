526bc95eb9a09e642937fa3ea5aecfd6dcba4718
demodevopsclass <akhil.prabha201@gmail.com>
2023-03-27 13:24:12 +0530

    Added chnages

--- user_account.py
+++ user_account.py
@@ -1,1 +1,5 @@
-print("dfdsaf")
+# Create User_Account
+# ITs should not add duliplicate
+# User id should Alpha numeric
+# Pass and user id should be len should be grater than 8
+# final out should be equal to the number of user in a list and smae lenght of password in Dict combination
