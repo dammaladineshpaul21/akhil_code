95c0221f6935f8d3a41b6fea0e7300a6f9072868
demodevopsclass <akhil.prabha201@gmail.com>
2023-03-28 01:43:21 +0530

    updated OTP feature

--- user_account.py
+++ user_account.py
@@ -8,6 +8,8 @@
 # After two user input's it should as the new user to contiue or to exit
 # Generate the OPT once user is created and ask the same after the passowrd entry
 
+import random
+
 user_name = []
 Id_password = []
 
@@ -17,13 +19,19 @@ for i in (range(3)):
 		if Ldap.isalpha() == Ldap.isnumeric():
 			if Ldap not in user_name:
 				user_name.append(Ldap)
+				random_number = random.randint(1000,9999)
+				print(f"{Ldap} your OTP is {random_number}")
 				pswd = input("enter password = ")
 				if len(pswd) >= 8:  
-					if pswd.isalpha() == pswd.isnumeric(): 
-						Id_password.append(pswd)
-						get_user_status = input("Do you want to contiue still = ")
-						if get_user_status.lower() == "no":
-							break
+					if pswd.isalpha() == pswd.isnumeric():
+						Ldap_OTP = int(input(f"enter the {Ldap} OTP = "))
+						if random_number == Ldap_OTP:
+							Id_password.append(pswd)
+							get_user_status = input("Do you want to contiue still = ")
+							if get_user_status.lower() == "no":
+								break
+						else:
+							print("Invalid OTP")
 					else:
 						print("password contain atleast one letter and number")
 				else:
