521f0d4652aad0f38c94d87c2d298606417ecda1
dammaladinshpaul21 <dammaladineshpaul21@gmail.com>
2023-03-28 14:20:29 +0530

    Added user readable commint to understand the code more detter

--- main.py
+++ main.py
@@ -6,21 +6,26 @@ password = []
 
 start_loop = True
 
+"""Account_Creation User Sitting"""
+
 while start_loop:
 	get_ldap = input("user ldap = ".upper())
-	if get_ldap not in user_id:
+	# Check whather the ID is existing or not in the list if not Added the user
+	if get_ldap not in user_id:   
 		if get_ldap.isalpha() == get_ldap.isnumeric():
 			user_id.append(get_ldap)
 			print(f"user_id create {get_ldap}")
+			# Ask the user for OTP and Password to get set the Account
 			while True:
-				var_otp = "dammala you OTP is " + str(random.randint(1111, 9999)) + " "
-				store_otp = int(var_otp[-5:-1])
+				store_otp = random.randint(1111, 9999)
 				print(f"{get_ldap} you OTP is {store_otp}")
 				passwd = input("Enter the password = ")
+				# Check the condition of the password policy one letter, number, Special Chareate
 				if len(passwd) >= 8 and len(re.findall("[a-z]", passwd)) >= 1 and \
 				len(re.findall("[A-Z]", passwd)) >= 1 and len(re.findall("[0-9]", passwd)) >= 1 and \
 				len(re.findall("[^[a-zA-Z0-9]", passwd)) >= 1:
 					user_otp = int(input("Enter you OTP = "))
+					# Check the run_time OTP and user_input OTP for verification.
 					if store_otp == user_otp:
 						password.append(passwd)
 						print(f"account create, {get_ldap}")
