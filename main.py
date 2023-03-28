480e7eb862b304edfa3ab170cec92b0d1a25e7a2
dammaladinshpaul21 <dammaladineshpaul21@gmail.com>
2023-03-28 16:33:37 +0530

    Add new function based on Password requirement

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import re
 user_id = []
 password = []
 
+
+def check_user_pass(user_id, password):
+	count = 0
+	for i in user_id:
+		if i in password:
+			count = count+1
+	# return count
+	if (len(user_id)/2)+2 <= count:
+		return False
+	else:
+		return True
+
 start_loop = True
 
 """Account_Creation User Sitting"""
@@ -20,29 +32,35 @@ while start_loop:
 				store_otp = random.randint(1111, 9999)
 				print(f"{get_ldap} you OTP is {store_otp}")
 				passwd = input("Enter the password = ")
+				if check_user_pass(get_ldap, passwd):
 				# Check the condition of the password policy one letter, number, Special Chareate
-				if len(passwd) >= 8 and len(re.findall("[a-z]", passwd)) >= 1 and \
-				len(re.findall("[A-Z]", passwd)) >= 1 and len(re.findall("[0-9]", passwd)) >= 1 and \
-				len(re.findall("[^[a-zA-Z0-9]", passwd)) >= 1:
-					user_otp = int(input("Enter you OTP = "))
-					# Check the run_time OTP and user_input OTP for verification.
-					if store_otp == user_otp:
-						password.append(passwd)
-						print(f"account create, {get_ldap}")
-						new_get_status = input("New_User Do you want to continue still Yes/No = ")
-						if new_get_status.lower() == "no":
-							start_loop = False
-							user_account_dict = dict(zip(user_id, password))
-                            print(user_account_dict)
-							break
+					if len(passwd) >= 8 and len(re.findall("[a-z]", passwd)) >= 1 and \
+					len(re.findall("[A-Z]", passwd)) >= 1 and len(re.findall("[0-9]", passwd)) >= 1 and \
+					len(re.findall("[^[a-zA-Z0-9]", passwd)) >= 1:
+						user_otp = int(input("Enter you OTP = "))
+						# Check the run_time OTP and user_input OTP for verification.
+						if store_otp == user_otp:
+							password.append(passwd)
+							print(f"account create, {get_ldap}")
+							new_get_status = input("New_User Do you want to continue still Yes/No = ")
+							if new_get_status.lower() == "no":
+								print(dict(zip(user_id, password)))
+								start_loop = False
+								break
+						else:
+							print(f"Invalid OTP {user_otp}")
+							# if store_otp != user_otp:
+							# 	print(f"Invalid OPT {user_otp}")
+	                        # else:
+	                        # 	print(f"The Max letter in the password are similar to {get_ldap}")
 					else:
-						print("Invalid OTP")
-				else:
-					print("password policy one letter, number, Special Chareate".upper())
-					get_status = input(f"{get_ldap} Do you want to continue still Yes/No = ")
-					if get_status.lower() == "no":
-						start_loop = False
+						print("password policy one letter, number, Special Chareate".upper())
+						get_status = input(f"{get_ldap} Do you want to continue still Yes/No = ")
+						if get_status.lower() == "no":
+							start_loop = False
 							print("We say sorry to missing you".upper())
+				else:
+					print(f"The Max letter in the password are similar to {get_ldap}")
 		else:
 			print(f"user_id should have condination of letter and number {get_ldap}")
 			get_status = input(f"{get_ldap} Do you want to continue still Yes/No = ")
@@ -51,5 +69,3 @@ while start_loop:
 				print("We say sorry to missing you".upper())
 	else:
 		print(f"{user_id} already taken, Try with newone")
-
-
--- rough.py
+++ rough.py
@@ -1,7 +1,21 @@
-import re
+# import re
 
-get_val = "Asadf2344@"
-if len(get_val) >= 8 and len(re.findall('[^a-zA-Z0-9]', get_val))>=1: 
-	print("Yes", len(re.findall("[a-z]", get_val)) >= 1 and len(re.findall("[A-Z]", get_val)) >= 1 and len(re.findall("[0-9]", get_val)) >= 1)
-else:
-	print("NO")
+# get_val = "Asadf2344@"
+# if len(get_val) >= 8 and len(re.findall('[^a-zA-Z0-9]', get_val))>=1: 
+# 	print("Yes", len(re.findall("[a-z]", get_val)) >= 1 and len(re.findall("[A-Z]", get_val)) >= 1 and len(re.findall("[0-9]", get_val)) >= 1)
+# else:
+# 	print("NO")
+
+# def check_user_pass(user_id, password):
+# 	count = 0
+# 	for i in user_id:
+# 		if i in password:
+# 			count = count+1
+# 	# return count
+# 	if len(user_id)/2 <= count:
+# 		return False
+# 	else:
+# 		return True
+
+
+# print(check_user_pass("Dammala4019", "adchd345"))
