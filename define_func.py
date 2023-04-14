61cfef0cee7870928706821a43f877e17b1d54a0
demodevopsclass <akhil.prabha201@gmail.com>
2023-04-14 19:14:52 +0530

    added func

--- define_func.py
+++ define_func.py
@@ -9,3 +9,28 @@ def id_name():
     return user_id
 
 print(id_name())
+
+
+def alpnum():
+    user_id = []
+    Ldap = input("enter the user_id = ")
+    if Ldap.isalpha() == Ldap.isnumeric():
+        user_id.append(Ldap)
+    else:
+        print("user id must contain alpha and numeric")
+    return user_id
+
+print(alpnum())
+
+
+def duplicate():
+    user_id = []
+    Ldap = input("enter user_id = ")
+    if Ldap not in user_id:
+        user_id.append(Ldap)
+    else:
+        print("user id already exist")
+        
+    return user_id
+
+print(duplicate())
